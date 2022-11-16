cbbc20be3ba332c3851f0c94119c293ae636a152
Tyrone Woody <bobbywoody7@gmail.com>
2022-11-16 00:21:50 -0700

    add

--- Cart.js
+++ Cart.js
@@ -1,22 +1,27 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import CartCards from "./CartCards";
+import { Button, TextInput } from "react-native-paper";
 
 const Cart = (props) => {
   let { cartItems, onAdd, onRemove } = props;
-
   let itemsPrice = 0;
+  const [code, setCode] = useState("");
+  const promoCode = code === "DIME" ? 0.9 : 1;
 
   cartItems.map(
     (el) => (itemsPrice += el.qty * parseInt(el.variants[0].price.amount))
   );
-
+  itemsPrice = itemsPrice * promoCode;
   const taxPrice = itemsPrice * 0.14;
   const shippingPrice = itemsPrice > 100 ? 0 : 20;
   const totalPrice = itemsPrice + taxPrice + shippingPrice;
 
+  // console.log(code === "DIME");
+
   return (
     <SafeAreaView style={styles.container}>
+      <Button onPress={() => 2}>Add to Bundle</Button>
       <Text style={styles.title}>Your Bag ({cartItems.length})</Text>
 
       {cartItems.length === 0 && (
@@ -27,7 +32,7 @@ const Cart = (props) => {
           <CartCards product={item} onAdd={onAdd} onRemove={onRemove} />
         </View>
       ))}
-
+      {/* Price */}
       <View>
         <Text style={styles.total}>
           Amount: ${cartItems.length === 0 ? 0.0 : itemsPrice.toFixed(2)}
@@ -41,6 +46,13 @@ const Cart = (props) => {
         <Text style={styles.totalAmount}>
           Total: ${cartItems.length === 0 ? 0.0 : totalPrice.toFixed(2)}
         </Text>
+        <TextInput
+          style={styles.code}
+          placeholder="code"
+          keyboardType="default"
+          maxLength={15}
+          onChangeText={(text) => setCode(text)}
+        />
       </View>
     </SafeAreaView>
   );
@@ -59,4 +71,5 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
   },
   empty: { fontSize: 24, alignSelf: "flex-end", margin: 20 },
+  code: { fontSize: 24, padding: 6 },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+Hi 👋🏽,
+
+Please install npm
+
+This app was created with expo you can use xcode (iphone simulator) or download the expo app and scan the qr code.
+
+this app was build with React Native
